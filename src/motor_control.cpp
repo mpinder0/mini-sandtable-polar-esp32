575d6ee85836a9b5ee14328e95dea540023280f4
Matt Pinder <pindycore@hotmail.com>
2025-12-23 18:56:02 +0000

    Add unit test guards to motor_control

--- src/motor_control.cpp
+++ src/motor_control.cpp
@@ -4,7 +4,8 @@
 MotorControl::MotorControl()
   : theta_timestamp_ms(0), rho_timestamp_ms(0), enabled(true)
 {
-  // Configure pins
+  // Configure pins (skipped under UNIT_TEST)
+#ifndef UNIT_TEST
   pinMode(REFERENCE_SENSOR_PIN, INPUT_PULLUP);
   pinMode(THETA_STEP_PIN, OUTPUT);
   pinMode(THETA_DIR_PIN, OUTPUT);
@@ -17,6 +18,7 @@ MotorControl::MotorControl()
   digitalWrite(RHO_STEP_PIN, LOW);
   digitalWrite(THETA_DIR_PIN, LOW);
   digitalWrite(RHO_DIR_PIN, LOW);
+#endif
 
   // Set timestamps so first step can occur immediately
   unsigned long now = millis();
@@ -31,8 +33,10 @@ MotorControl::~MotorControl() {}
 void MotorControl::set_enable(bool enable)
 {
   enabled = enable;
+#ifndef UNIT_TEST
   // Motor drivers are enabled with LOW (active low enable pin)
   digitalWrite(MOTORS_ENABLE_PIN, enable ? LOW : HIGH);
+#endif
 }
 
 void MotorControl::motors_release()
@@ -65,12 +69,16 @@ void MotorControl::theta_step(bool reverse)
 
   if (AXIS_T_INVERT_DIR) reverse = !reverse;
 
+#ifndef UNIT_TEST
   digitalWrite(THETA_DIR_PIN, !reverse);
 
   // Pulse the step pin
   digitalWrite(THETA_STEP_PIN, HIGH);
   delayMicroseconds(STEP_PULSE_US);
   digitalWrite(THETA_STEP_PIN, LOW);
+#else
+  printf("theta step %s\n", reverse ? "reverse" : "forward");
+#endif
 
   theta_timestamp_ms = millis();
 }
@@ -87,17 +95,26 @@ void MotorControl::rho_step(bool reverse)
 
   if (AXIS_R_INVERT_DIR) reverse = !reverse;
 
+#ifndef UNIT_TEST
   digitalWrite(RHO_DIR_PIN, !reverse);
 
   digitalWrite(RHO_STEP_PIN, HIGH);
   delayMicroseconds(STEP_PULSE_US);
   digitalWrite(RHO_STEP_PIN, LOW);
+#else
+  printf("Rho step %s\n", reverse ? "reverse" : "forward");
+#endif
 
   rho_timestamp_ms = millis();
 }
 
 bool MotorControl::get_reference_sensor_value() const
 {
+#ifndef UNIT_TEST
   // Sensor is active LOW
   return digitalRead(REFERENCE_SENSOR_PIN) == LOW;
+#else
+  // Under unit tests there is no hardware; return a safe default (not triggered)
+  return false;
+#endif
 }
