a6570e96b02dcf0c3b9e8d5c81b0467434596be4
Matt Pinder <pindycore@hotmail.com>
2026-01-02 22:51:28 +0000

    Add mover class logic

--- include/step_motion_mover.h
+++ include/step_motion_mover.h
@@ -3,49 +3,27 @@
 
 #include <Arduino.h>
 #include "motor_control.h"
+#include "step_motion_planner.h"
+#include <vector>
 
-class StepMotionMover {
+class StepMotionMover
+{
 public:
-  StepMotionMover(MotorControl& motor_control);
-  ~StepMotionMover();
- 
-  void play();
-  void play_move();
+    StepMotionMover(MotorControl &motor_control, StepMotionPlanner &motion_planner);
+    ~StepMotionMover();
+
+    void set_pattern(std::vector<Coordinates> &new_pattern);
+    void play_pattern();
+    void play_move(Move move);
 
 private:
-  unsigned long theta_timestamp_ms;
+    unsigned long theta_next_ts;
+    unsigned long rho_next_ts;
+    MotorControl &motor_control;
+    StepMotionPlanner &planner;
+    std::vector<Coordinates> pattern;
 
+    bool at_target_timestamp(unsigned long current_time, unsigned long target_time);
 };
 
 #endif // MOVER_H
-
-
-/*
-    def play_move(self, move):
-        '''
-        params
-        move - dict:
-            start_position - tuple (theta, rho) current position
-            end_position - tuple (theta, rho) next position
-            axis_steps_list - list of tuples (theta, rho) contraining step True/False for each time step
-            directions - tuple F/R for each axis
-        '''
-        axis_steps_list = move['axis_steps_list']
-        directions = move['directions']
-
-        # iterate through the axis steps list
-        for step in axis_steps_list:
-            # execute the axis steps
-            self._play_both_axis_step(directions, step)
-            # wait for time step
-            time.sleep(TIME_STEP_S)
-        
-        self.current_position = move["end_position"]
-    
-    def play(self, pattern):
-        # loop through position changes
-        for position in pattern:
-            move = self.get_steps_for_move(position)
-            # execute motor steps to complete the move
-            self.play_move(move)
-*/
---
+++ src/step_motion_mover.cpp
@@ -0,0 +1,80 @@
+#include "step_motion_mover.h"
+#include "step_motion_planner.cpp"
+#include <cmath>
+
+StepMotionMover::StepMotionMover(MotorControl &motor_control, StepMotionPlanner &step_motion_planner)
+    : theta_next_ts(0), rho_next_ts(0), motor_control(motor_control), planner(step_motion_planner)
+{
+    // pass
+}
+
+StepMotionMover::~StepMotionMover()
+{
+    pattern.clear();
+}
+
+void StepMotionMover::set_pattern(std::vector<Coordinates> &new_pattern)
+{
+    pattern = new_pattern;
+}
+
+void StepMotionMover::play_pattern()
+{
+    // Loop while moves available in pattern
+    for (size_t i = 0; i < pattern.size() - 1; ++i)
+    {
+        // Get move from current to next position
+        Move mv = planner.get_move_steps(pattern[i], pattern[i + 1]);
+        // Execute the move
+        play_move(mv);
+    }
+}
+
+bool StepMotionMover::at_target_timestamp(unsigned long current_time, unsigned long target_time)
+{
+    // Handle wrap-around by using time delta, unsigned arithmetic
+    const auto delta = target_time - current_time;
+    // Cast as signed to test for negative (overflowed)
+    return static_cast<int32_t>(delta) <= 0;
+}
+
+void StepMotionMover::play_move(Move move)
+{
+    unsigned long start_ts = millis();
+    unsigned long current_ts = start_ts;
+    unsigned long end_ts = start_ts + move.moveTime;
+
+    // Calculate step intervals for each axis
+    unsigned long theta_interval = (move.thetaSteps > 0) ? move.moveTime / move.thetaSteps : move.moveTime;
+    unsigned long rho_interval = (move.rhoSteps > 0) ? move.moveTime / move.rhoSteps : move.moveTime;
+
+    unsigned long next_theta_ts = start_ts;
+    unsigned long next_rho_ts = start_ts;
+
+    int theta_steps_done = 0;
+    int rho_steps_done = 0;
+
+    while (current_ts < end_ts)
+    {
+        current_ts = millis();
+
+        // Step theta axis if it's time
+        if (theta_steps_done < move.thetaSteps && at_target_timestamp(current_ts, next_theta_ts))
+        {
+            motor_control.step(THETA, move.thetaDir == BACKWARD);
+            theta_steps_done++;
+            next_theta_ts += theta_interval;
+        }
+
+        // Step rho axis if it's time
+        if (rho_steps_done < move.rhoSteps && at_target_timestamp(current_ts, next_rho_ts))
+        {
+            motor_control.step(RHO, move.rhoDir == BACKWARD);
+            rho_steps_done++;
+            next_rho_ts += rho_interval;
+        }
+
+        // Small delay to prevent tight loop
+        delayMicroseconds(500); // 0.5 ms
+    }
+}
